23b26f002f045998255cb9e397f5326640a0829d
Shashank <srishashankjampala@gmail.com>
2026-08-14 11:13:06 +0530

    hii

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,43 @@
-alert("HELLO FROM JAVASCRIPT")
+const users = [
+ {
+   "name" : "Jonn Doe",
+   "gender" : "Male",
+   "image" : "john.png"
+ },
+   {
+   "name" : "Jane Doe",
+   "gender" : "Female",
+   "image" : "jane.png"
+ }
+]
+
+
+var curIndex = 0;
+
+
+function toggle(){
+ if(curIndex == 0)
+   curIndex = 1;
+ else
+   curIndex = 0;
+
+
+ document.getElementById("card-image").src = users[curIndex].image
+ document.getElementById("card-name").innerText =users[curIndex].name
+ document.getElementById("card-gender").innerText = users[curIndex].gender
+
+}
+function random(){
+	fetch("https://randomuser.me/api")
+	.then(function(response){
+        return response.json();
+    })
+   .then(function(data){
+var  detail = data.results[0];
+document.getElementById("card-image").src = detail.picture.large;
+document.getElementById("card-gender").innerText = detail.gender;
+var fullname =detail.name.title + " " +detail.name.first+" "+detail.name.last;
+document.getElementById("card-name").innerText = fullname;
+});
+}
+    
